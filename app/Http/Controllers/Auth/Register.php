cdab5045d65d29af8d08ae437725027e62497eca
Anon Dev <michaelerastus9@gmail.com>
2025-01-31 18:32:21 +0100

    Made USD the default currency

--- app/Http/Controllers/Auth/Register.php
+++ app/Http/Controllers/Auth/Register.php
@@ -43,7 +43,7 @@ class Register extends Controller
             'pageName'=>'Account Registration',
             'referral'=>$request->get('referral'),
             'countries' => Country::all(),
-            'currencies' => Country::groupBy('currency')->get(),
+            'currencies' => Country::where('currency','USD')->get(),
         ];
 
         return view('auth.register',$dataView);
